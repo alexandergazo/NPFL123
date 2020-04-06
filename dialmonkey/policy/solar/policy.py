6146918527e030995d3e019adfb912e294ab249f
Jonas Kulhanek <jonas.kulhanek@live.com>
2020-04-06 23:44:51 +0200

    impove solar nlu and add tests

--- dialmonkey/policy/solar/policy.py
+++ dialmonkey/policy/solar/policy.py
@@ -1,13 +1,79 @@
-from ...component import Component
-from ...utils import choose_one
 from .repository import SolarRepository
 from itertools import groupby
 from inspect import signature
-from ...da import DA, DAI
+from dialmonkey.da import DA, DAI
+from dialmonkey.component import Component
+from dialmonkey.dialogue import Dialogue
+from dialmonkey.utils import choose_one
 
 class RequestQueryMapper:
     def __init__(self, repo):
-        self._repo = repo
+        self._repo: SolarRepository = repo
+        self._property_map = repo.properties()
+
+    def greet(self):
+        return ('greet', None, None)
+
+    def request_single(self, property, object):
+        bodies = self._repo.bodies()
+        if object == 'planet':
+            bodies = (x for x in bodies if x['isPlanet'])
+        elif object == 'solar_body':
+            pass
+        elif object == 'gas_giant':
+            bodies = (x for x in bodies if x['planetType'] == 'gas_giant')
+        else:
+            return [('not_implemented', None, None)]
+
+        append_info = None
+        if property == 'largest':
+            body = max(bodies, key=lambda x: x['meanRadius'])
+            append_info = ('inform', 'radius', f"{body['meanRadius']:.0f}km")
+        elif property == 'closest':
+            return [('not_implemented', None, None)]
+        elif property == 'furthest':
+            return [('not_implemented', None, None)] 
+
+        result = [('inform', 'object', object), ('inform', 'property', property), ('inform', 'name', body['englishName'])]
+        if append_info is not None:
+            result.append(append_info)
+        return result
+
+    def request(self, name):
+        body = self._repo.body(name)
+        return [('inform', 'name', body['englishName']), ('inform','gravity',f"{body['gravity']:0.2f}g"), f"{body['meanRadius']:.0f}km")]
+
+    def request_property(self, name, property):
+        append_info = []
+        intent = 'inform'
+        body = self._repo.body(name)
+        if property in self._property_map:
+            pname, format = self._property_map[property]
+            append_info = [(intent, property, format % body[pname])]
+        elif property == 'discovery':
+            append_info = [(intent, 'discovered_by', body['discoveredBy']), (intent, 'discovered_by', body['discoveryDate'])]
+        else:
+            return None
+
+        result = [(intent, 'name', body['englishName'])] 
+        result.extend(append_info)
+        return result
+    
+    def count_moons(self, name):
+        bodies = [x for x in bodies if x['isPlanet'] and x['englishName'].lower() == name]
+        if len(bodies) == 0:
+            return [('inform_unknown_planet', 'name', name)] 
+        planet = bodies[0] 
+        moons = len(planet['moons']) 
+        return [('inform_moons', 'count', f'{moons}'), ('inform_moons', 'moons', ','.join((x['moon'] for x in planet['moons'])))]
+
+    def request_moons(self, name):
+        bodies = [x for x in bodies if x['isPlanet'] and x['englishName'].lower() == name]
+        if len(bodies) == 0:
+            return [('inform_unknown_planet', 'name', name)] 
+        planet = bodies[0] 
+        moons = len(planet['moons']) 
+        return [('inform_moons', 'count', f'{moons}'), ('inform_moons', 'moons', ','.join((x['moon'] for x in planet['moons'])))]
 
     def __call__(self, intent, **kwargs):
         if not hasattr(self, intent): return None
@@ -15,7 +81,7 @@ class RequestQueryMapper:
         required = [x for x,y in signature(method).parameters.items() if y.default is not None]
         for r in required:
             if r not in kwargs: return ('request', r, None)
-        return method(**{k:v for k,v in kwargs.items() if k in method.parameters }) 
+        return method(**{k:v for k,v in kwargs.items() if k in signature(method).parameters }) 
 
 class SolarPolicy(Component):
     def __init__(self, config=None):
@@ -24,24 +90,41 @@ class SolarPolicy(Component):
         self._repository = SolarRepository()
         self._mapper = RequestQueryMapper(self._repository)
 
-    def _map_call(self, da: DA, state):
+    def _map_call(self, da: DA, state) -> DA:
         assert da is not None
         assert isinstance(da, DA)
         assert state is not None
 
         dai = [(x.intent, x.confidence) for x in da.dais]
         dai.sort()
-        dai = [(k, sum(lambda x: x[1], s)) for k,s in groupby(dai, lambda x: x[0])]
+        dai = [(k, sum(map(lambda x: x[1], s))) for k,s in groupby(dai, key=lambda x: x[0])]
         dai.sort(reverse=True)
 
         # We support single intent for now
         intent, _ = dai[0]
         values = ((k, max((p, v) for v, p in x.items())) for k,x in state.items())
-        values = { k: v for k, (p, v) in values if p > self._treshold }
-        return self._mapper(intent, **values)
+        values = { k: v for k, (p, v) in values if p > self._treshold and isinstance(k, str) }
+        response = self._mapper(intent, **values)
+        if response is None: response = list() 
+        elif not isinstance(response, list): response = [response]
 
-    def __call__(self, dial, logger):
-        return self._map_call(dial['user'], dial['state'])
+        # Map to DA
+        da = DA()
+        for r in response:
+            da.append(DAI(*r))
+        return da
+
+    def __call__(self, dial: Dialogue, logger):
+        response: DA = self._map_call(dial['nlu'], dial['state'])
+
+        # TODO: will fill the response for the next component 
+        # in the pipeline as soon as the Dialogue object
+        # supports it. For now, we will just return the 
+        # response as a string
+        dial.set_system_response(response.to_cambridge_da_string())
+        if any((x for x in response.dais if x.intent == 'exit')):
+            dial.end_dialogue()
+        return dial
 
     def reset(self):
         pass
--- dialmonkey/repositories/solar.py
+++ dialmonkey/repositories/solar.py
@@ -6,15 +6,27 @@ class SolarRepository:
     def __init__(self):
         self._bodies = None
         self._details = dict()
+        self._gas_giants = ['jupiter', 'saturn','uranus','neptune']
         pass 
 
     def bodies(self):
         if self._bodies is None:
             bodies = requests.get(SolarRepository._url + '/bodies')
-            self._bodies = bodies.json()
+            self._bodies = bodies.json()['bodies']
+            for b in self._bodies:
+                if b['isPlanet'] and b['englishName'].lower() in self._gas_giants:
+                    b['planetType'] = 'gas_giant'
+                elif b['isPlanet']:
+                    b['planetType'] = 'planet'
         return self._bodies
 
     def body(self, id):
         if not id in self._details:
             response = requests.get(SolarRepository._url + f'/bodies/{id}')
             self._details[id] = response.json()
+
+    def properties(self): 
+        return dict(
+            gravity=('gravity', '%.1fg'),
+            radius=('meanRadius', '%.0fkm'),
+        )
