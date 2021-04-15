97d8da2212a48a9c4750146fa37b1193ce0832ca
alexander <alexander@gazo.sk>
2021-04-15 23:05:48 +0200

    cleanup

--- dialmonkey/dst/rule.py
+++ dialmonkey/dst/rule.py
@@ -7,11 +7,14 @@ class DST(Component):
     def __call__(self, dial, logger):
         if dial.state is None: dial.state = dict()
         nlu: DA = dial.nlu
-        # ignore intents I guess
+        # ignore intents I guess from the assignment
         slot_value_p = [(x.slot, x.value, x.confidence) for x in nlu.dais]
         slot_value_p.sort(key=lambda x: tuple(map(str, x)))
-        # slot_value_p = [(slot, value, sum(map(lambda x: x[-1], x)))
-        #                 for (slot,value), x in groupby(slot_value_p, key=lambda x: tuple(map(str, x[:-1])))]
+
+        # I don't think the slot confidence should be summed up, but this
+        # should not occur in the data
+        # slot_value_p = [(slot, value, sum(map(lambda x: x[-1], x))) for (slot,value), x in groupby(slot_value_p, key=lambda x: tuple(map(str, x[:-1])))]
+
         for slot, values in groupby(slot_value_p, key=lambda x: str(x[0])):
             if not slot in dial.state: dial.state[slot] = { None: 1.0 }
             conf = dial.state[slot]
